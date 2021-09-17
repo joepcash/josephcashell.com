be34b4e54b8fd694fce695bb511f74c6a6dc2656
Joseph Cashell <joepcash@gmail.com>
2021-09-17 15:42:57 +0700

    Cleaned up work experience component

--- frontend/src/app/work-positions/work-position-item/work-position-item.component.ts
+++ frontend/src/app/work-positions/work-position-item/work-position-item.component.ts
@@ -2,6 +2,7 @@ import { NONE_TYPE } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
 import { Experience } from 'src/app/shared/experience.model';
 import { WorkPosition } from 'src/app/shared/work-position.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-work-position-item',
@@ -10,11 +11,20 @@ import { WorkPosition } from 'src/app/shared/work-position.model';
 })
 export class WorkPositionItemComponent implements OnInit {
   @Input() work_position!: WorkPosition;
+  datepipe: DatePipe = new DatePipe('en-US');
 
   constructor() {
    }
 
   ngOnInit(): void {
+  }
+
+  cleanDate(date: Date) {
+    if (date) {
+      return this.datepipe.transform(date, 'MMM y');
+    } else {
+      return 'present';
+    }
   }
 
 }
